922198e194e64f6c0c3f145a3c6dd1b23d04950e
bbye-rim <yelim3907@gmail.com>
2022-05-17 13:52:28 +0900

    검색창 추가 및 앱 로고 변경

--- app/src/main/java/com/team3/showbee/ui/view/UserLeaveDialog.kt
+++ app/src/main/java/com/team3/showbee/ui/view/UserLeaveDialog.kt
@@ -1,11 +1,12 @@
-package com.team3.showbee
+package com.team3.showbee.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.team3.showbee.R
 
 class user_leave : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_leave)
+        setContentView(R.layout.dialog_user_leave)
     }
 }
