1db2b4a63f4676f8fe875efa8d5ba5bd9eea18f9
nayeon0731 <toothless1596@gmail.com>
2022-06-20 19:46:29 +0900

    create UpdateScheduleActivity

---
+++ app/src/main/java/com/team3/showbee/ui/view/UpdateScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.team3.showbee.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.team3.showbee.R
+
+class UpdateScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_schedule)
+    }
+}
